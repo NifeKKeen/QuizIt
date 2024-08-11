1e4ff96fdbc389ed68e373880a04ff255025a47c
Kanat <110170587+NifeKKeen@users.noreply.github.com>
2024-08-12 02:05:17 +0500

    Alpha 0.1.2

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { Suspense } from "react";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
-import { NotFoundPageAsync } from "./pages/NotFound/NotFoundPageAsync";
+import { NotFoundPageAsync } from "./pages/NotFoundPage/NotFoundPageAsync";
 
 export function App() {
   return (
