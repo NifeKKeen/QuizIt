f12bbc159b9d691694e8dbfc65196141f0fe52b5
Kanat <110170587+NifeKKeen@users.noreply.github.com>
2024-11-11 20:57:52 +0500

    Add styles to Header and NavigationList components

--- src/shared/components/NavigationList.jsx
+++ src/shared/components/NavigationList.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from "react-router-dom";
+import styles from "./NavigationList.module.sass";
 
-export function NavigationList() {
+console.log(styles);
+
+export function NavigationList({ direction = "horizontal"  }) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to={"/"}>Home</NavLink>
+    <nav className={styles.nav}>
+      <ul className={direction === "horizontal" ? styles.horizontalUl : styles.verticalUl}>
+        <li className={styles.horizontalUlLi}>
+          <NavLink to={"/"} className={styles.horizontalUlLiA}>Home</NavLink>
         </li>
-        <li>
-          <NavLink to={"/about"}>About</NavLink>
+        <li className={styles.horizontalUlLi}>
+          <NavLink to={"/about"} className={styles.horizontalUlLiA}>About</NavLink>
         </li>
       </ul>
     </nav>
